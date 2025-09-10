9a86076cb7fd7ec5f6939546cbbb88d588d6f6c9
henke164 <henrik.mattsson@hotmail.com>
2025-09-10 19:37:42 +0200

    serve FE

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,15 +2,22 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import routes from "./routes";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3001;
+const frontendPath = path.join(__dirname, "../../frontend/dist");
+app.use(express.static(frontendPath));
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
 
 app.get("/", (req, res) => {
   res.sendStatus(200);
